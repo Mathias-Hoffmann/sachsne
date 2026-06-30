406d0361bc34863188743abbf9cd52eda8aa91cb
Mathias Hoffmann <Mathias.Hoffmann@volvo.com>
2026-06-30 08:49:12 +0200

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/hoffmannmathias/', // ⚠️ très important pour GitHub Pages
+  base: '/sachsne/', // ⚠️ très important pour GitHub Pages
 })
 
 
